ee44bf4e855994532713644ff23160239f4649bc
sourabhxyz <lambdaproofs@pm.me>
2025-03-21 19:19:36 +0530

    feat(#16): incorporate atlas's option to provide extra tx build configuration

--- atlas/zkfold-smart-wallet-server-lib/src/ZkFold/Cardano/SmartWallet/Server/Ctx.hs
+++ atlas/zkfold-smart-wallet-server-lib/src/ZkFold/Cardano/SmartWallet/Server/Ctx.hs
@@ -2,15 +2,21 @@ module ZkFold.Cardano.SmartWallet.Server.Ctx (
   Ctx (..),
   runSkeletonI,
   runSkeletonWithStrategyI,
+  runSkeletonWithExtraConfigurationI,
+  runSkeletonWithStrategyAndExtraConfigurationI,
   runSkeletonF,
   runSkeletonWithStrategyF,
+  runSkeletonWithExtraConfigurationF,
+  runSkeletonWithStrategyAndExtraConfigurationF,
   runQuery,
   runQueryWithReader,
 ) where
 
 import Control.Monad.Reader (ReaderT (..))
+import Data.Default (Default (..))
 import GeniusYield.Imports
 import GeniusYield.Transaction
+import GeniusYield.Transaction.Common (GYTxExtraConfiguration)
 import GeniusYield.TxBuilder
 import GeniusYield.Types
 import ZkFold.Cardano.SmartWallet.Types (ZKWalletBuildInfo)
@@ -51,6 +57,32 @@ runSkeletonWithStrategyI ::
   IO GYTxBody
 runSkeletonWithStrategyI cstrat = coerce (runSkeletonWithStrategyF @Identity cstrat)
 
+-- | Create 'TxBody' from a 'GYTxSkeleton', with the specified extra transaction building configuration.
+runSkeletonWithExtraConfigurationI ::
+  GYTxExtraConfiguration v ->
+  Ctx ->
+  -- | User's used addresses. Note that internally we prepend given change address to this list so that in case wallet's state isn't updated quickly to mark an earlier given change address as used, we'll be able to use UTxOs potentially present at this change address.
+  [GYAddress] ->
+  -- | User's change address.
+  GYAddress ->
+  -- | User's collateral.
+  Maybe GYTxOutRef ->
+  ReaderT ZKWalletBuildInfo GYTxBuilderMonadIO (GYTxSkeleton v) ->
+  IO GYTxBody
+runSkeletonWithExtraConfigurationI ec = coerce (runSkeletonWithExtraConfigurationF @Identity ec)
+
+-- | Create 'TxBody' from a 'GYTxSkeleton', with the specified coin selection strategy and extra transaction building configuration.
+runSkeletonWithStrategyAndExtraConfigurationI ::
+  GYCoinSelectionStrategy ->
+  GYTxExtraConfiguration v ->
+  Ctx ->
+  [GYAddress] ->
+  GYAddress ->
+  Maybe GYTxOutRef ->
+  ReaderT ZKWalletBuildInfo GYTxBuilderMonadIO (GYTxSkeleton v) ->
+  IO GYTxBody
+runSkeletonWithStrategyAndExtraConfigurationI cstrat ec = coerce (runSkeletonWithStrategyAndExtraConfigurationF @Identity cstrat ec)
+
 runSkeletonF ::
   (Traversable t) =>
   Ctx ->
@@ -62,7 +94,7 @@ runSkeletonF ::
   Maybe GYTxOutRef ->
   ReaderT ZKWalletBuildInfo GYTxBuilderMonadIO (t (GYTxSkeleton v)) ->
   IO (t GYTxBody)
-runSkeletonF = runSkeletonWithStrategyF GYRandomImproveMultiAsset
+runSkeletonF = runSkeletonWithStrategyF def
 
 runSkeletonWithStrategyF ::
   (Traversable t) =>
@@ -76,7 +108,36 @@ runSkeletonWithStrategyF ::
   Maybe GYTxOutRef ->
   ReaderT ZKWalletBuildInfo GYTxBuilderMonadIO (t (GYTxSkeleton v)) ->
   IO (t GYTxBody)
-runSkeletonWithStrategyF cstrat ctx addrs addr mcollateral skeleton = do
+runSkeletonWithStrategyF cstrat = runSkeletonWithStrategyAndExtraConfigurationF cstrat def
+
+runSkeletonWithExtraConfigurationF ::
+  (Traversable t) =>
+  GYTxExtraConfiguration v ->
+  Ctx ->
+  -- | User's used addresses. Note that internally we prepend given change address to this list so that in case wallet's state isn't updated quickly to mark an earlier given change address as used, we'll be able to use UTxOs potentially present at this change address.
+  [GYAddress] ->
+  -- | User's change address.
+  GYAddress ->
+  -- | User's collateral.
+  Maybe GYTxOutRef ->
+  ReaderT ZKWalletBuildInfo GYTxBuilderMonadIO (t (GYTxSkeleton v)) ->
+  IO (t GYTxBody)
+runSkeletonWithExtraConfigurationF = runSkeletonWithStrategyAndExtraConfigurationF def
+
+runSkeletonWithStrategyAndExtraConfigurationF ::
+  (Traversable t) =>
+  GYCoinSelectionStrategy ->
+  GYTxExtraConfiguration v ->
+  Ctx ->
+  -- | User's used addresses. Note that internally we prepend given change address to this list so that in case wallet's state isn't updated quickly to mark an earlier given change address as used, we'll be able to use UTxOs potentially present at this change address.
+  [GYAddress] ->
+  -- | User's change address.
+  GYAddress ->
+  -- | User's collateral.
+  Maybe GYTxOutRef ->
+  ReaderT ZKWalletBuildInfo GYTxBuilderMonadIO (t (GYTxSkeleton v)) ->
+  IO (t GYTxBody)
+runSkeletonWithStrategyAndExtraConfigurationF cstrat ec ctx addrs addr mcollateral skeleton = do
   let nid = ctxNetworkId ctx
       providers = ctxProviders ctx
       wi = ctxSmartWalletBuildInfo ctx
@@ -84,7 +145,7 @@ runSkeletonWithStrategyF cstrat ctx addrs addr mcollateral skeleton = do
         collateral <- mcollateral
         pure (collateral, False)
 
-  runGYTxMonadNodeF cstrat nid providers (addr : addrs) addr mcollateral' $ runReaderT skeleton wi
+  runGYTxMonadNodeF cstrat ec nid providers (addr : addrs) addr mcollateral' $ runReaderT skeleton wi
 
 runQuery :: Ctx -> ReaderT ZKWalletBuildInfo GYTxQueryMonadIO a -> IO a
 runQuery ctx = runQueryWithReader ctx (ctxSmartWalletBuildInfo ctx)
@@ -95,5 +156,16 @@ runQueryWithReader ctx a q = do
       providers = ctxProviders ctx
   runGYTxQueryMonadIO nid providers $ runReaderT q a
 
-runGYTxMonadNodeF :: forall t v. (Traversable t) => GYCoinSelectionStrategy -> GYNetworkId -> GYProviders -> [GYAddress] -> GYAddress -> Maybe (GYTxOutRef, Bool) -> GYTxBuilderMonadIO (t (GYTxSkeleton v)) -> IO (t GYTxBody)
-runGYTxMonadNodeF strat nid providers addrs change collateral act = runGYTxBuilderMonadIO nid providers addrs change collateral $ act >>= traverse (buildTxBodyWithStrategy strat)
+runGYTxMonadNodeF ::
+  forall t v.
+  (Traversable t) =>
+  GYCoinSelectionStrategy ->
+  GYTxExtraConfiguration v ->
+  GYNetworkId ->
+  GYProviders ->
+  [GYAddress] ->
+  GYAddress ->
+  Maybe (GYTxOutRef, Bool) ->
+  GYTxBuilderMonadIO (t (GYTxSkeleton v)) ->
+  IO (t GYTxBody)
+runGYTxMonadNodeF strat ec nid providers addrs change collateral act = runGYTxBuilderMonadIO nid providers addrs change collateral $ act >>= traverse (buildTxBodyWithStrategyAndExtraConfiguration strat ec)
