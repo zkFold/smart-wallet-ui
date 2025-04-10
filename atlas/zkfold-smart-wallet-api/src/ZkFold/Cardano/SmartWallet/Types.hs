6b2ec1ea5de0ed9f6f00db55098cc122569a2e94
sourabhxyz <lambdaproofs@pm.me>
2025-04-10 19:31:47 +0530

    feat(#25): add zkproofbytes type

--- atlas/zkfold-smart-wallet-api/src/ZkFold/Cardano/SmartWallet/Types.hs
+++ atlas/zkfold-smart-wallet-api/src/ZkFold/Cardano/SmartWallet/Types.hs
@@ -12,19 +12,29 @@ module ZkFold.Cardano.SmartWallet.Types (
   Email,
   emailFromText,
   emailToText,
+  ZKF (..),
+  ZKProofBytes (..),
+  zkProofToPlutus,
 ) where
 
 import Control.Exception (Exception)
 import Control.Monad.Reader (MonadReader)
+import Data.Aeson (withText)
+import Data.ByteString.Base16 qualified as BS16
+import Data.Swagger qualified as Swagger
 import Data.Text (Text)
 import Data.Text qualified as Text
 import GHC.Generics (Generic)
 import GeniusYield.HTTP.Errors (GYApiError (..), IsGYApiError (..))
-import GeniusYield.Imports (FromJSON, ToJSON, coerce)
+import GeniusYield.Imports (FromJSON (..), ToJSON, coerce, encodeUtf8, (&), (<&>))
+import GeniusYield.Swagger.Utils
 import GeniusYield.TxBuilder (GYTxQueryMonad)
 import GeniusYield.Types (GYMintingPolicyId, GYPaymentKeyHash, GYPubKeyHash, GYScript, GYScriptHash, GYStakeAddress, PlutusVersion (..))
+import GeniusYield.Types.OpenApi ()
 import Network.HTTP.Types (status400, status500)
-import ZkFold.Cardano.OnChain.Plonkup.Data (ProofBytes)
+import PlutusTx.Builtins qualified as PlutusTx
+import ZkFold.Cardano.OnChain.BLS12_381.F (F (..))
+import ZkFold.Cardano.OnChain.Plonkup.Data (ProofBytes (..))
 import ZkFold.Cardano.UPLC.Wallet.Types
 
 -- | Information required to build transactions for a zk-wallet.
@@ -44,6 +54,11 @@ newtype JWT = JWT Text
   deriving stock (Eq, Ord, Show, Generic)
   deriving newtype (FromJSON, ToJSON)
 
+instance Swagger.ToSchema JWT where
+  declareNamedSchema =
+    Swagger.genericDeclareNamedSchema Swagger.defaultSchemaOptions
+      & addSwaggerDescription "JSON web token"
+
 jwtFromText :: Text -> JWT
 jwtFromText = coerce
 
@@ -55,7 +70,7 @@ data ZKCreateWalletInfo = ZKCreateWalletInfo
   { zkcwiEmail :: !Email
   , zkcwiJWT :: !JWT
   , zkcwiPaymentKeyHash :: !GYPaymentKeyHash
-  , zkcwiProofBytes :: !ProofBytes
+  , zkcwiProofBytes :: !ZKProofBytes
   }
 
 data ZKInitializedWalletScripts = ZKInitializedWalletScripts
@@ -113,11 +128,120 @@ instance IsGYApiError ZKWalletException where
 -- | Email address.
 newtype Email = Email Text
   deriving stock (Eq, Ord, Show, Generic)
-  deriving newtype (FromJSON, ToJSON)
+  deriving newtype (ToJSON)
+
+instance FromJSON Email where
+  parseJSON =
+    withText "Email" $
+      either (fail . show) return . emailFromText
+
+instance Swagger.ToSchema Email where
+  declareNamedSchema =
+    Swagger.genericDeclareNamedSchema Swagger.defaultSchemaOptions
+      & addSwaggerDescription "User's email address."
+      & addSwaggerExample "zkfold@gmail.com"
 
 -- TODO: To validate for emails? Or specifically gmails?
-emailFromText :: Text -> Email
-emailFromText = coerce
+
+-- | Validates and returns for email.
+emailFromText :: Text -> Either String Email
+emailFromText = Right . coerce
 
 emailToText :: Email -> Text
 emailToText = coerce
+
+-- | Field element.
+newtype ZKF = ZKF Integer
+  deriving stock (Show, Eq, Ord, Generic)
+  deriving newtype (FromJSON, ToJSON)
+
+instance Swagger.ToSchema ZKF where
+  declareNamedSchema =
+    Swagger.genericDeclareNamedSchema Swagger.defaultSchemaOptions
+      & addSwaggerDescription "Field element."
+
+-- | ZK proof bytes, assuming hex encoding for relevant bytes.
+data ZKProofBytes = ZKProofBytes
+  { cmA_bytes_hex :: !Text
+  , cmB_bytes_hex :: !Text
+  , cmC_bytes_hex :: !Text
+  , cmF_bytes_hex :: !Text
+  , cmH1_bytes_hex :: !Text
+  , cmH2_bytes_hex :: !Text
+  , cmZ1_bytes_hex :: !Text
+  , cmZ2_bytes_hex :: !Text
+  , cmQlow_bytes_hex :: !Text
+  , cmQmid_bytes_hex :: !Text
+  , cmQhigh_bytes_hex :: !Text
+  , proof1_bytes_hex :: !Text
+  , proof2_bytes_hex :: !Text
+  , a_xi_int :: !Integer
+  , b_xi_int :: !Integer
+  , c_xi_int :: !Integer
+  , s1_xi_int :: !Integer
+  , s2_xi_int :: !Integer
+  , f_xi_int :: !Integer
+  , t_xi_int :: !Integer
+  , t_xi'_int :: !Integer
+  , z1_xi'_int :: !Integer
+  , z2_xi'_int :: !Integer
+  , h1_xi'_int :: !Integer
+  , h2_xi_int :: !Integer
+  , l1_xi :: !ZKF
+  }
+  deriving stock (Show, Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+instance Swagger.ToSchema ZKProofBytes where
+  declareNamedSchema =
+    Swagger.genericDeclareNamedSchema Swagger.defaultSchemaOptions
+      & addSwaggerDescription "Proof bytes where bytes are represented in hexadecimal encoding."
+
+zkProofToPlutus :: ZKProofBytes -> Either String ProofBytes
+zkProofToPlutus ZKProofBytes{..} = do
+  cmA <- hexToBytes cmA_bytes_hex
+  cmB <- hexToBytes cmB_bytes_hex
+  cmC <- hexToBytes cmC_bytes_hex
+  cmF <- hexToBytes cmF_bytes_hex
+  cmH1 <- hexToBytes cmH1_bytes_hex
+  cmH2 <- hexToBytes cmH2_bytes_hex
+  cmZ1 <- hexToBytes cmZ1_bytes_hex
+  cmZ2 <- hexToBytes cmZ2_bytes_hex
+  cmQlow <- hexToBytes cmQlow_bytes_hex
+  cmQmid <- hexToBytes cmQmid_bytes_hex
+  cmQhigh <- hexToBytes cmQhigh_bytes_hex
+  proof1 <- hexToBytes proof1_bytes_hex
+  proof2 <- hexToBytes proof2_bytes_hex
+
+  pure $
+    ProofBytes
+      { cmA_bytes = cmA
+      , cmB_bytes = cmB
+      , cmC_bytes = cmC
+      , cmF_bytes = cmF
+      , cmH1_bytes = cmH1
+      , cmH2_bytes = cmH2
+      , cmZ1_bytes = cmZ1
+      , cmZ2_bytes = cmZ2
+      , cmQlow_bytes = cmQlow
+      , cmQmid_bytes = cmQmid
+      , cmQhigh_bytes = cmQhigh
+      , proof1_bytes = proof1
+      , proof2_bytes = proof2
+      , a_xi_int = a_xi_int
+      , b_xi_int = b_xi_int
+      , c_xi_int = c_xi_int
+      , s1_xi_int = s1_xi_int
+      , s2_xi_int = s2_xi_int
+      , f_xi_int = f_xi_int
+      , t_xi_int = t_xi_int
+      , t_xi'_int = t_xi'_int
+      , z1_xi'_int = z1_xi'_int
+      , z2_xi'_int = z2_xi'_int
+      , h1_xi'_int = h1_xi'_int
+      , h2_xi_int = h2_xi_int
+      , l1_xi = coerce l1_xi
+      }
+ where
+  hexToBytes :: Text -> Either String PlutusTx.BuiltinByteString
+  hexToBytes t = BS16.decode (encodeUtf8 t) <&> PlutusTx.toBuiltin
