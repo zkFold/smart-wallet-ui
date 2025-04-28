04f8a9b543e7cf4d806753ec0147a5bfc4efe83d
sourabhxyz <lambdaproofs@pm.me>
2025-04-28 11:48:57 +0530

    feat: remove Haskell server code

--- atlas/zkfold-smart-wallet-api/src/ZkFold/Cardano/SmartWallet/Types/Monad.hs
+++
@@ -1,9 +0,0 @@
-module ZkFold.Cardano.SmartWallet.Types.Monad (
-  ZKWalletQueryMonad,
-) where
-
-import Control.Monad.Reader (MonadReader)
-import GeniusYield.TxBuilder (GYTxSpecialQueryMonad)
-import ZkFold.Cardano.SmartWallet.Types.Script (ZKWalletBuildInfo)
-
-type ZKWalletQueryMonad m = (GYTxSpecialQueryMonad m, MonadReader ZKWalletBuildInfo m)
