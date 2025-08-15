ea44a277beccd7eb812ca7f967b93a970fd7cf6c
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-08-15 13:18:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
+  base: '/smart-wallet-ui/',
   plugins: [
     wasm(),
     topLevelAwait()
