0f9a44780848ef809353e324806293d4619a42ab
Eugene Samodelov <geli1610@yandex.ru>
2025-12-30 12:02:12 +0300

    draft: add manifest and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import manifest from './manifest.json'
+import { crx } from '@crxjs/vite-plugin'
 
 export default defineConfig({
   plugins: [
     wasm(),
-    topLevelAwait()
+    topLevelAwait(),
+    crx({ manifest })
   ],
   build: {
     outDir: './dist',
@@ -21,6 +24,12 @@ export default defineConfig({
         changeOrigin: true,
         secure: false
       }
+    },
+    cors: {
+      origin: "*",     // РАЗРЕШАЕМ ВСЕ (или можно 'chrome-extension://...')
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      optionsSuccessStatus: 204
     }
   },
   define: {
